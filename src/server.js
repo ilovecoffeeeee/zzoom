5bc672aedf3401b2e990d792aaa6d760be6453fd
sun_loves_Q <conconxel@gmail.com>
2021-10-31 00:47:30 +0900

    code challenge

--- src/server.js
+++ src/server.js
@@ -19,13 +19,24 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
     socket.onAny((event) => {
         console.log(`Socket Event : ${event}`);
     });
-    socket.on("enter_room", (roomName, done) => {
+    socket.on("enter_room", (roomName, nickname, done) => {
+        socket["nickname"] = nickname;
         socket.join(roomName);
         done();
+        socket.to(roomName).emit("welcome",socket.nickname);
     });
+    socket.on("disconnecting", ()=> {
+        socket.rooms.forEach(room => socket.to(room).emit("bye",socket.nickname));
+    });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 // WebSocket
